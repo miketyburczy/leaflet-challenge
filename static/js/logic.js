84f21a7a4d6a39278b54974d9ff2886072d916c4
Mike Tyburczy <miketyburczy@mikes-mbp.lan>
2021-06-01 16:21:02 -0400

    progress

---
+++ static/js/logic.js
@@ -0,0 +1,21 @@
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+function markerSize(magnitude) {
+    return magnitude * 10000;
+}
+
+function markerCol(magnitude) {
+    if (magnitude <1) {
+        return "#FFFF00";
+    } else if (magnitude < 2) {
+        return "#8B8000";
+    } else if (magnitude < 3) {
+        return "#FFA500";
+    } else if (magnitude < 4) {
+        return "##ff8c00";
+    } else if (magnitude < 5) {
+        return "#ff0000";
+    } else {
+        return "#8b0000";
+    };
+}
